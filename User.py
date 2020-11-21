4191f89049d6bed9070d1ac0b61eeb681484e999
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-21 17:34:05 +0300

    add initialization method in user class

---
+++ User.py
@@ -0,0 +1,15 @@
+class User:
+    """
+    Class that generates new instances of Users.
+    """
+
+    def _init_(self, username, password):
+        '''
+        Method that helps us define properties for our objects.
+        
+        Args:
+            username: New User username.
+            password: New User password.
+    
+        '''
+           
