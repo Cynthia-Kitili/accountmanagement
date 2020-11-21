32bde6ece8205eb2c08b85a7c4b1c6ead77e333e
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-21 17:54:48 +0300

    add set up method

--- account_test.py
+++ account_test.py
@@ -5,4 +5,12 @@ class Testuser(unittest.TestCase):
     '''
     Test class that defines test for the user class behaviours.
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_User= user("CindyNyambu","fox12345")
+
+        
     
