34bcf72cad24a6e633f96ff7b5636f38aadb9352
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-22 21:11:00 +0300

    add find method in credentials file

--- credentials.py
+++ credentials.py
@@ -30,6 +30,15 @@ class Credentials:
         '''
         Credentials.credentials_list.remove(self) 
 
+    @classmethod
+    def find_by_account(cls,account):
+        '''
+        Method that takes in an acount and returns credentials that matches that account.
+        '''
+        for credentials in cls.credentials_list:
+            if credentials.account==account:
+                return credentials    
+
 
 
     
