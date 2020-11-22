3a89c92bc61e5a34149b02715e8311147a525fa8
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-22 19:42:38 +0300

    add init method in crdentilas file

--- credentials.py
+++ credentials.py
@@ -1,1 +1,23 @@
 class Credentials:
+    '''
+    Class that generates new instances of credentials.
+    '''
+    credentials_list = []
+    
+    def __init__(self,account,name,password):
+        '''
+        Method that helps us define properties for our objects.
+
+        Args:
+           account: New user account.
+           name: New user name.
+           password: New user password.
+        '''
+
+        self.account= account
+        self.name=name
+        self.password
+        
+
+    
+
