d0a19d061c76a0e6cdd50f0397d445101615e8a8
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-23 18:22:25 +0300

    login user

--- access.py
+++ access.py
@@ -20,3 +20,11 @@ def display_user():
     Function to display existing user
     """
     return User.display_user()      
+
+def login_user(username,password):
+    """
+    function that checks whether a user exist and then login the user in.
+    """
+  
+    confirm_user = Credentials.verify_user(username,password)
+    return confirm_user    
